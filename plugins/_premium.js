c1e25dbd3761d10240a12d1996c53e05c9f20702
Aldair828 <aldairdurand6@gmail.com>
2024-10-08 11:03:54 -0500

    Update _premium.js

--- plugins/_premium.js
+++ plugins/_premium.js
@@ -1,10 +1,40 @@
-let handler = m => m
-export async function all(m) {
-let user = global.db.data.users[m.sender]
-if (m.chat.endsWith('broadcast')) return
-if (user.premiumTime != 0 && user.premium) {
-if (new Date() * 1 >= user.premiumTime) {
-await m.reply(`🚩 @${m.sender.split`@`[0]} se terminó tu tiempo como usuario *Premium.*`, false, { mentions: [m.sender] })
-user.premiumTime = 0
-user.premium = false 
-}}}
+import { promises } from 'fs'
+import { join } from 'path'
+
+let handler = async (m, { conn }) => {
+  try {
+    // Lista de URLs de videos HTTPS
+    const videos = [
+      "https://qu.ax/beuXG.mp4",
+      "https://qu.ax/WcuJg.mp4",
+      "https://qu.ax/bVTZR.mp4",
+      "https://qu.ax/SMJnR.mp4",
+      "https://qu.ax/AwlFy.mp4",
+      "https://qu.ax/hSPHX.mp4", 
+    ]
+
+    // Selecciona un video aleatorio
+    let randomVideo = videos[Math.floor(Math.random() * videos.length)]
+
+    // Mensaje
+    let message = "🥵 Aquí está tu vídeo pajero 🥵"
+
+    // Enviar el video con el mensaje
+    await conn.sendMessage(m.chat, {
+      video: { url: randomVideo }, // Video aleatorio de la lista
+      caption: message              // Mensaje de texto junto con el video
+    }, { quoted: m })
+
+  } catch (e) {
+    conn.reply(m.chat, 'Lo sentimos, hubo un error al enviar el video.', m)
+    throw e
+  }
+}
+
+// Definir los comandos
+handler.help = ['porno']
+handler.tags = ['media']
+handler.command = ['porno'] // El comando será '.video'
+handler.register = true
+
+export default handler
