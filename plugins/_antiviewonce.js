4fb224df852264590f957719ef2d1fb5f778aab7
Aldair828 <aldairdurand6@gmail.com>
2024-10-10 02:33:14 -0500

    Update _antiviewonce.js

--- plugins/_antiviewonce.js
+++ plugins/_antiviewonce.js
@@ -1,24 +1,32 @@
-export async function before(m, { conn, isAdmin, isBotAdmin }) {
-    if (m.isBaileys && m.fromMe) return !0;
-    if (!m.isGroup) return !1;
+let { downloadContentFromMessage } = (await import(global.baileys))
 
-    let chat = global.db.data.chats[m.chat];
-    if (chat.antiviewonce && m.message && m.message.viewOnceMessage) {
-        let viewOnceMessage = m.message.viewOnceMessage;
-        let mediaMessage = viewOnceMessage.message.imageMessage || viewOnceMessage.message.videoMessage;
+let handler = m => m
+handler.before = async function (m, { conn, isAdmin, isBotAdmin }) {
+let media, msg, type
+const { antiver, isBanned } = global.db.data.chats[m.chat]
+if (!antiver  isBanned  !(m.mtype == 'viewOnceMessageV2' || m.mtype == 'viewOnceMessageV2Extension')) return
+if (m.mtype == 'viewOnceMessageV2' || m.mtype == 'viewOnceMessageV2Extension') {
+msg = m.mtype == 'viewOnceMessageV2' ? m.message.viewOnceMessageV2.message : m.message.viewOnceMessageV2Extension.message 
+type = Object.keys(msg)[0]
+if (m.mtype == 'viewOnceMessageV2') {
+media = await downloadContentFromMessage(msg[type], type == 'imageMessage' ? 'image' : 'videoMessage' ? 'video' : 'audio')
+} else {
+media = await downloadContentFromMessage(msg[type], 'audio')
+}
+let buffer = Buffer.from([])
+for await (const chunk of media) {
+buffer = Buffer.concat([buffer, chunk])}
+const fileSize = formatFileSize(msg[type].fileLength)
+const description = mid.antiviewonce(type, fileSize, m, msg)
+if (/image|video/.test(type)) return await conn.sendFile(m.chat, buffer, type == 'imageMessage' ? 'error.jpg' : 'error.mp4', description, m, false, { mentions: [m.sender] })
+if (/audio/.test(type)) { 
+await conn.reply(m.chat, description, m, { mentions: [m.sender] }) 
+await conn.sendMessage(m.chat, { audio: buffer, fileName: 'error.mp3', mimetype: 'audio/mpeg', ptt: true }, { quoted: m })
+}}}
+export default handler
 
-        if (mediaMessage) {
-            let mediaType = mediaMessage.mimetype.startsWith('image') ? 'imagen' : 'video';
-            let buffer = await conn.downloadMediaMessage(mediaMessage);
-
-            await conn.sendMessage(m.chat, {
-                [mediaMessage.mimetype.startsWith('image') ? 'image' : 'video']: buffer,
-                caption: `*Este ${mediaType} estaba en modo "Ver una vez", pero ahora puedes verlo cuantas veces quieras.*`
-            });
-
-            await conn.reply(m.chat, `*El ${mediaType} de "Ver una vez" ha sido interceptado y convertido en un mensaje normal.*`, m);
-        }
-    }
-
-    return !0;
+function formatFileSize(bytes) {
+const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'TY', 'EY']
+const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)))
+return Math.round(100 * (bytes / Math.pow(1024, i))) / 100 + ' ' + sizes[i]
 }
