b15f647863a2f3604877a3197aab25eda92265eb
Aldair828 <aldairdurand6@gmail.com>
2024-10-11 02:00:39 -0500

    Update rg-unreg.js

--- plugins/rg-unreg.js
+++ plugins/rg-unreg.js
@@ -1,9 +1,9 @@
 import { createHash } from 'crypto'
 let handler = async function (m, { conn, args, usedPrefix}) {
-  if (!args[0]) return m.reply(`🚩 Ingresa tu número de serie junto al comando.`)
+  if (!args[0]) return m.reply('*`Ingresa tu número de serie junto con el comando`*')
   let user = global.db.data.users[m.sender]
   let sn = createHash('md5').update(m.sender).digest('hex')
-  if (args[0] !== sn) return m.reply('🚩 Número de serie incorrecto')
+  if (args[0] !== sn) return m.reply('*`Número de serie incorrecto`*')
   user.registered = false
   m.reply(`> *Se elimino su registro de mi base 🐦‍🔥`)
 }
