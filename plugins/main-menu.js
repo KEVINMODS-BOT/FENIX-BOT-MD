28932e6f5250ba907b4139b32370540622be8435
Aldair828 <aldairdurand6@gmail.com>
2024-10-11 00:35:23 -0500

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, db }) => {
   try {
     let name = await conn.getName(m.sender)
     
-    // Contar usuarios registrados
+    
     let registeredUsers = Object.values(global.db.data.users).filter(user => user.registered).length;
 
     let menuText = `
@@ -25,21 +25,13 @@ let handler = async (m, { conn, db }) => {
 └──┸─┸┸─┸─┸┸┸┸─┙
 `.trim()
 
-    let thumbnailBuffer = await fetchBuffer("https://qu.ax/GsHEY.jpg");
+    let imageBuffer = await fetchBuffer("https://qu.ax/GsHEY.jpg");
 
+    // Enviar imagen junto con el menú
     await conn.sendMessage(m.chat, { 
-      text: menuText, 
-      contextInfo: { 
-        externalAdReply: {
-          title: "Haz clic para ver el canal",
-          body: "Canal oficial de WhatsApp",
-          mediaType: 1,
-          mediaUrl: "https://whatsapp.com/channel/0029VapwUi0Dp2QC3xO9PX42",
-          sourceUrl: "https://whatsapp.com/channel/0029VapwUi0Dp2QC3xO9PX42",
-          thumbnail: thumbnailBuffer
-        }
-      }
-    }, { quoted: m })
+      image: imageBuffer, 
+      caption: menuText
+    }, { quoted: m });
 
   } catch (e) {
     conn.reply(m.chat, 'Lo sentimos, el menú tiene un error.', m)
