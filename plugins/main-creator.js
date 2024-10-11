f95744f8273c543ceeb366a0ef15ee0bd6948263
KEVINMODS <kevinvallejos689@gmail.com>
2024-10-11 01:48:52 -0300

    Update main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,9 +1,9 @@
 
 
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
-    let vcard1 = `BEGIN:VCARD\nVERSION:3.0\nN:𝐀𝐋𝐃𝐀𝐈𝐑 👨🏻‍💻;;\nFN:𝐀𝐋𝐃𝐀𝐈𝐑 👨🏻‍💻 \nORG:𝐂𝐑𝐄𝐀𝐃𝐎𝐑 𝐎𝐅𝐂 👁‍🗨\nTITLE:\nitem1.TEL;waid=51925015528:+51925015528\nitem1.X-ABLabel:𝐀𝐋𝐃𝐀𝐈𝐑 👨🏻‍💻\nX-WA-BIZ-DESCRIPTION:𝙳𝚄𝙳𝙰𝚂 𝙴𝚂𝙲𝚁𝙸𝙱𝙸𝚁𝙼𝙴 👇🏻\nX-WA-BIZ-NAME:𝐀𝐋𝐃𝐀𝐈𝐑 👨🏻‍💻\nEND:VCARD`;
+    let vcard1 = `BEGIN:VCARD\nVERSION:3.0\nN:𝑲𝑬𝑽𝑰𝑵 👨🏻‍💻;;\nFN:𝑲𝑬𝑽𝑰𝑵𝑴𝑶𝑫𝑺 👨🏻‍💻 \nORG:𝐂𝐑𝐄𝐀𝐃𝐎𝐑 𝐎𝐅𝐂 👁‍🗨\nTITLE:\nitem1.TEL;waid=5493624187763:+5493624187763\nitem1.X-ABLabel:𝑲𝑬𝑽𝑰𝑵𝑴𝑶𝑫𝑺 👨🏻‍💻\nX-WA-BIZ-DESCRIPTION:𝙳𝚄𝙳𝙰𝚂 𝙴𝚂𝙲𝚁𝙸𝙱𝙸𝚁𝙼𝙴 👇🏻\nX-WA-BIZ-NAME:𝐀𝐋𝐃𝐀𝐈𝐑 👨🏻‍💻\nEND:VCARD`;
 
-    let vcard2 = `BEGIN:VCARD\nVERSION:3.0\nN:𝑲𝑬𝑽𝑰𝑵 𝑺𝑶𝑷𝑶𝑹𝑻;;\nFN:𝑲𝑬𝑽𝑰𝑵 𝑺𝑶𝑷𝑶𝑹𝑻\nORG:𝚂𝙾𝙿𝙾𝚁𝚃𝙴 𝙱𝙾𝚃 🌐\nTITLE:\nitem1.TEL;waid=5493624187763:+5493624187763\nitem1.X-ABLabel:𝑲𝑬𝑽𝑰𝑵 𝑺𝑶𝑷𝑶𝑹𝑻\nX-WA-BIZ-DESCRIPTION:𝑲𝑬𝑽𝑰𝑵 𝑺𝑶𝑷𝑶𝑹𝑻\nX-WA-BIZ-NAME:𝑲𝑬𝑽𝑰𝑵 𝑺𝑶𝑷𝑶𝑹𝑻\nEND:VCARD`;
+    let vcard2 = `BEGIN:VCARD\nVERSION:3.0\nN:𝑨𝑳𝑫𝑨𝑰𝑹 👨🏻‍💻;;\nFN:𝑨𝑳𝑫𝑨𝑰𝑹 𝑪𝑶𝑳𝑨𝑩👨🏻‍💻\nORG:𝚂𝙾𝙿𝙾𝚁𝚃𝙴 𝙱𝙾𝚃 🌐\nTITLE:\nitem1.TEL;waid=51925015528:+51925015528\nitem1.X-ABLabel:𝑨𝑳𝑫𝑨𝑰𝑹 👨🏻‍💻\nX-WA-BIZ-DESCRIPTION:𝚎𝚛𝚛𝚘𝚛𝚎𝚜 𝚎𝚜𝚌𝚛𝚒𝚋𝚒𝚛𝚖𝚎 👇🏻\nX-WA-BIZ-NAME:𝑨𝑳𝑫𝑨𝑰𝑹 𝑪𝑶𝑳𝑨𝑩 👨🏻‍💻\nEND:VCARD`;
 
     await conn.sendMessage(m.chat, {
         contacts: {
