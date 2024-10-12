aa24c0fe784459ab8cf052eb1bf686a32b8e9c2e
Aldair828 <aldairdurand6@gmail.com>
2024-10-12 01:04:53 -0500

    Update downloader-tiktokvid.js

--- plugins/downloader-tiktokvid.js
+++ plugins/downloader-tiktokvid.js
@@ -5,16 +5,20 @@ let handler = async (m, { conn, usedPrefix, command, text, args }) => {
 
   // Verificar si el usuario tiene al menos 2 fuegos
   if (user.fuegos < 2) {
+    // Si no tiene suficientes fuegos, enviar un mensaje de advertencia
     return conn.reply(m.chat, '*`No tienes suficientes fuegos 🔥 para usar este comando.`*\n\n *`Necesitas 2 fuegos.`*', m);
   }
 
-  if (!text) return conn.reply(m.chat, '🚩 Ingresa un texto junto al comando.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* Ai Hoshino Edit`, m, rcanal);
+  // Verificar si se ha proporcionado texto para la búsqueda
+  if (!text) return conn.reply(m.chat, '🚩 Ingresa un texto junto al comando.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* Fenix Edit`, m, rcanal);
   
   await m.react('🕓');
 
   try {
+    // Llamar al scraper para obtener los datos del video
     let { title, author, duration, views, likes, comments_count, share_count, download_count, published, dl_url } = await Starlights.tiktokvid(text);
 
+    // Formatear el texto del mensaje con los detalles del video
     let txt = '`乂  T I K T O K  -  D O W N L O A D`\n\n';
     txt += `*➢ Título* : ${title}\n`;
     txt += `*➢ Autor* : ${author}\n`;
@@ -27,11 +31,14 @@ let handler = async (m, { conn, usedPrefix, command, text, args }) => {
     // Descontar 2 fuegos al usuario
     user.fuegos -= 2;
 
+    // Enviar el video junto con el texto
     await conn.sendFile(m.chat, dl_url, `thumbnail.mp4`, txt, m);
     await m.react('✅');
 
-  } catch {
+  } catch (e) {
+    // Manejo de error en caso de fallo en la descarga
     await m.react('✖️');
+    conn.reply(m.chat, '❌ Ocurrió un error al intentar descargar el vídeo.', m);
   }
 }
 
