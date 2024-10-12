f1d7953dea5a44f4d60caca083297c1c4221ca6c
Aldair828 <aldairdurand6@gmail.com>
2024-10-12 01:12:14 -0500

    Update downloader-fb.js

--- plugins/downloader-fb.js
+++ plugins/downloader-fb.js
@@ -1,18 +1,40 @@
 import Starlights from '@StarlightsTeam/Scraper'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args || !args[0]) return conn.reply(m.chat, '🚩 Ingresa el enlace del vídeo de Facebook junto al comando.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* https://www.facebook.com/official.trash.gang/videos/873759786348039/?mibextid=rS40aB7S9Ucbxw6v`, m, rcanal)
-await m.react('🕓')
-try {
-let { dl_url } = await Starlights.fbdl(args[0])
-await conn.sendFile(m.chat, dl_url, 'fbdl.mp4', listo, m, null, rcanal)
-await m.react('✅')
-} catch {
-await m.react('✖️')
-}}
-handler.help = ['fb *<link fb>*']
-handler.tags = ['downloader'] 
-handler.command = /^(facebook|fb|facebookdl|fbdl)$/i
-//handler.limit = 1
-handler.register = true
-export default handler
+    let user = global.db.data.users[m.sender];
+
+    // Verificar si el usuario tiene al menos 1 fuego
+    if (!user.fuegos || user.fuegos < 1) {
+        return conn.reply(m.chat, '*`No tienes suficientes fuegos para usar este comando.`*\n\n *`Necesitas al menos 1 fuego.`*', m);
+    }
+
+    // Verificar si se proporciona el enlace de Facebook
+    if (!args || !args[0]) {
+        return conn.reply(m.chat, '🚩 Ingresa el enlace del vídeo de Facebook junto al comando.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* https://www.facebook.com/official.trash.gang/videos/873759786348039/?mibextid=rS40aB7S9Ucbxw6v`, m);
+    }
+
+    await m.react('🕓');
+
+    try {
+        // Obtener el enlace de descarga del video de Facebook
+        let { dl_url } = await Starlights.fbdl(args[0]);
+
+        // Descontar 1 fuego al usuario
+        user.fuegos -= 1;
+
+        // Enviar el video descargado
+        await conn.sendFile(m.chat, dl_url, 'fbdl.mp4', 'Descarga completada. Haz gastado 1 fuego 🔥', m);
+        await m.react('✅');
+    } catch (e) {
+        // Manejo de error si ocurre un problema durante la descarga
+        await m.react('✖️');
+        conn.reply(m.chat, '❌ Ocurrió un error al intentar descargar el vídeo de Facebook.', m);
+    }
+};
+
+handler.help = ['fb *<link fb>*'];
+handler.tags = ['downloader'];
+handler.command = /^(facebook|fb|facebookdl|fbdl)$/i;
+handler.register = true;
+
+export default handler;
