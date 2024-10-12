5746a1b3e89bc8e79d355a053e0d9293b428d95c
Aldair828 <aldairdurand6@gmail.com>
2024-10-12 01:13:58 -0500

    Update downloader-pinterest.js

--- plugins/downloader-pinterest.js
+++ plugins/downloader-pinterest.js
@@ -1,25 +1,42 @@
 import Starlights from '@StarlightsTeam/Scraper';
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) return conn.reply(m.chat,`🚩 Ingrese un enlace de Pinterest\n\nEjemplo:\n> *${usedPrefix + command}* https://ar.pinterest.com/pin/588142032613788991/`, m);
+    let user = global.db.data.users[m.sender];
 
-await m.react('🕓');
-try {
-let { dl_url, quality, size, duration, url } = await Starlights.pinterestdl(args[0]);
+    // Verificar si el usuario tiene al menos 1 fuego
+    if (!user.fuegos || user.fuegos < 1) {
+        return conn.reply(m.chat, '*`No tienes suficientes fuegos para usar este comando.`*\n\n *`Necesitas al menos 1 fuego.`*', m);
+    }
 
-let txt = '`乂  P I N T E R E S T  -  D L`\n\n'
-txt += `  ✩   *Calidad* : ${quality}\n`;
-txt += `  ✩   *Tamaño* : ${size}\n`;
-txt += `  ✩   *Duracion* : ${duration}\n`;
-txt += `  ✩   *Url* : ${url}\n\n`
-txt += `> 🚩 *${textbot}*`
+    // Verificar si se proporciona el enlace de Pinterest
+    if (!args[0]) {
+        return conn.reply(m.chat, `🚩 Ingrese un enlace de Pinterest\n\nEjemplo:\n> *${usedPrefix + command}* https://ar.pinterest.com/pin/588142032613788991/`, m);
+    }
 
+    await m.react('🕓');
+    try {
+        // Obtener los datos del video de Pinterest
+        let { dl_url, quality, size, duration, url } = await Starlights.pinterestdl(args[0]);
 
-await conn.sendMessage(m.chat, { video: { url: dl_url }, caption: txt, mimetype: 'video/mp4', fileName:  `pinterest.mp4`}, {quoted: m })
-await m.react('✅');
-} catch {
-await m.react('✖️');
-}
+        // Formatear el mensaje con los detalles del video
+        let txt = '`乂  P I N T E R E S T  -  D L`\n\n';
+        txt += `  ✩   *Calidad* : ${quality}\n`;
+        txt += `  ✩   *Tamaño* : ${size}\n`;
+        txt += `  ✩   *Duración* : ${duration}\n`;
+        txt += `  ✩   *Url* : ${url}\n\n`;
+        txt += `> 🚩 Haz gastado 1 fuego 🔥`;
+
+        // Descontar 1 fuego al usuario
+        user.fuegos -= 1;
+
+        // Enviar el video descargado
+        await conn.sendMessage(m.chat, { video: { url: dl_url }, caption: txt, mimetype: 'video/mp4', fileName: 'pinterest.mp4' }, { quoted: m });
+        await m.react('✅');
+    } catch (e) {
+        // Manejo de error si ocurre un problema durante la descarga
+        await m.react('✖️');
+        conn.reply(m.chat, '❌ Ocurrió un error al intentar descargar el vídeo de Pinterest.', m);
+    }
 };
 
 handler.help = ['pinterestdl *<url pin>*'];
